aa9a733920c419d1be39bbfadaf4aca21306579e
Mikhail Tsapkov <matsapkov@gmail.com>
2024-04-29 21:12:11 +0300

    Выполнено второе задание из ДЗ

--- project_tracker/quality_control/admin.py
+++ project_tracker/quality_control/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from .models import BugReport, FeatureRequest
+
+
+@admin.register(BugReport)
+class BugReportAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'project', 'status', 'priority', 'created_at', 'updated_at')
+    list_filter = ('status', 'priority', 'project')
+    search_fields = ('title', 'description')
+
+    actions = ['mark_completed']
+
+    def mark_completed(self, request, queryset):
+        queryset.update(status='Completed')
+
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'description', 'project', 'task')
+        }),
+        ('Status and Priority', {
+            'fields': ('status', 'priority')
+        }),
+    )
+
+@admin.register(FeatureRequest)
+class FeatureRequestAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'project', 'status', 'priority', 'created_at', 'updated_at')
+    list_filter = ('status', 'priority', 'project')
+    search_fields = ('title', 'description')
+
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'description', 'project', 'task')
+        }),
+        ('Status and Priority', {
+            'fields': ('status', 'priority')
+        }),
+    )
+
 
 # Register your models here.
